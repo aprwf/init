e1d696bb7f34dd0575045a4d758c16240e380fa6
aprwf <38931872+aprwf@users.noreply.github.com>
2020-06-13 18:52:24 -0700

    add style, component and page examples (#4)
    * add style, component and page examples
    
    * fixed missing css jest config
    
    * add newlines
    
    Co-authored-by: Angel <>

---
+++ components/hf-title/HFTitle.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import styles from './HFTitle.module.css'
+
+export default function HFTitle() {
+    return (<h1 className={styles.title1}>
+        GLHF
+    </h1>)
+}
---
+++ pages/gg.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import styles from './styles/gg.module.css'
+
+export default () => (
+    <h1 className={styles.title1}>
+        GG
+    </h1>
+)
