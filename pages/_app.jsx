72d5b5b311a80d050e6dd8155c2bce76acaa8d3f
Angel <>
2020-06-13 18:50:20 -0700

    add newlines

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,5 +3,5 @@ import React from 'react'
 import globals from './styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />
-  }
+  return <Component {...pageProps} />
+}
